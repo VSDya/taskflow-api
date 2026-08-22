3d0c82a3292caadd9b698ea5e7d5dab98c39302a
VSDya <125130637+VSDya@users.noreply.github.com>
2026-08-22 18:12:30 +0300

    feat: add task not found exception

---
+++ src/main/java/com/vsdya/taskflow/task/application/TaskNotFoundException.java
@@ -0,0 +1,9 @@
+package com.vsdya.taskflow.task.application;
+
+import java.util.UUID;
+
+public class TaskNotFoundException extends RuntimeException {
+    public TaskNotFoundException(UUID id) {
+        super("Task not found: " + id);
+    }
+}
