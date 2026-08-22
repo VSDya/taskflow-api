8af9beafae2e5c63eaa97d57502aebf8d53a4c2c
VSDya <125130637+VSDya@users.noreply.github.com>
2026-08-22 17:59:54 +0300

    feat: add project JPA repository

---
+++ src/main/java/com/vsdya/taskflow/project/infrastructure/ProjectRepository.java
@@ -0,0 +1,8 @@
+package com.vsdya.taskflow.project.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProjectRepository extends JpaRepository<ProjectEntity, UUID> {
+}
